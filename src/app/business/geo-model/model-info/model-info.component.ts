8b819e0b0c57a54a262a0d063c0f0197a06f5278
SunsetFrost <710851944@qq.com>
2018-02-01 15:32:56 +0800

    更改模型详情页

--- src/app/business/geo-model/model-info/model-info.component.ts
+++ src/app/business/geo-model/model-info/model-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 import { MockService } from "../../mock/mock.service";
 import { Model } from "../../mock/model.model";
@@ -12,22 +13,99 @@ import { Task } from "../../mock/task.model";
   styleUrls: ['./model-info.component.scss']
 })
 export class ModelInfoComponent implements OnInit {
+  _isLoading = true;
+
+  _allChecked = false;
+  _disabledButton = true;
+  _checkedNumber = 0;
+  _displayData: Array<any> = [];
+  _operating = false;
+  _dataSet = [];
+  _indeterminate = false;
+
   model: Model;
   models: Model[];
   datas: Data[];
   solutions: Solution[];
   tasks: Task[];
 
-  constructor(private mockService: MockService) { 
+  constructor(
+    private mockService: MockService,
+    private route: ActivatedRoute
+  ) { 
     this.models = mockService.getModels();
-    this.model = this.models[0];
+    this.model = this.models[1];
     this.datas = mockService.getDatas();
     this.solutions = mockService.getSolutions();
-    console.log(this.solutions);
     this.tasks = mockService.getTask();
   }
 
   ngOnInit() {
+    this._isLoading = false;
+
+    this.route.params
+      .subscribe((params: Params) => {
+
+      });
+
+    //temp mock
+    for (let i = 0; i < 46; i++) {
+      this._dataSet.push({
+        key    : i,
+        name   : `NPP ${i}`,
+        unit    : 'km/s',
+        desc: `This is very detail description. ${i}`,
+      });
+    }
   }
 
+  _displayDataChange($event) {
+    this._displayData = $event;
+  }
+
+  _refreshStatus() {
+    const allChecked = this._displayData.every(value => value.checked === true);
+    const allUnChecked = this._displayData.every(value => !value.checked);
+    this._allChecked = allChecked;
+    this._indeterminate = (!allChecked) && (!allUnChecked);
+    this._disabledButton = !this._dataSet.some(value => value.checked);
+    this._checkedNumber = this._dataSet.filter(value => value.checked).length;
+  }
+
+  _checkAll(value) {
+    if (value) {
+      this._displayData.forEach(data => data.checked = true);
+    } else {
+      this._displayData.forEach(data => data.checked = false);
+    }
+    this._refreshStatus();
+  }
+
+  _operateData() {
+    this._operating = true;
+    setTimeout(_ => {
+      this._dataSet.forEach(value => value.checked = false);
+      this._refreshStatus();
+      this._operating = false;
+    }, 1000);
+  }
+
+  @HostListener('scroll')
+  onScroll(event: any) {
+      // console.log('onScroll');
+      // const scrollH = Math.max(
+      //     window.pageYOffset,
+      //     window.scrollY,
+      //     document.documentElement.scrollTop,
+      //     document.body.scrollTop
+      // );
+      const scrollH = jQuery('#root-container')[0].scrollTop;
+      const h = jQuery('#separator')[0].offsetTop - scrollH;
+      console.log(h);
+      if (h < -50) {
+          jQuery('.side-catalog').css('visibility', 'visible');
+      } else {
+          jQuery('.side-catalog').css('visibility', 'hidden');
+      }
+  }
 }
