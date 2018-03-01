23668fbb47693115c056ec9e83ad5c64b75bad24
shenchaoran <shenchaoran212@gmail.com>
2018-03-01 14:24:12 +0800

    change: router tree, scrollbar style

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,20 +4,75 @@ import { ModuleWithProviders } from '@angular/core';
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: './business/webNJGIS.module#WebNJGISModule'
+        redirectTo: 'home',
+        pathMatch: 'full'
     },
     {
         path: 'login',
         loadChildren: './common/feature/login/login.module#LoginModule'
     },
     {
+        path: 'home',
+        loadChildren: './business/home/home.module#HomeModule'
+    },
+    {
+        path: 'resources',
+        children: [
+            {
+                path: '',
+                redirectTo: 'geo-models',
+                pathMatch: 'full'
+            },
+            {
+                path: 'geo-models',
+                loadChildren: './business/geo-model/geo-model.module#GeoModelModule'
+            },
+            {
+                path: 'geo-data',
+                loadChildren: './business/geo-data/geo-data.module#GeoDataModule'
+            },
+        ]
+    },
+    {
+        path: 'issues',
+        loadChildren: './business/cmp-issue/cmp-issue.module#CmpIssueModule'
+    },
+    {
+        path: 'solutions',
+        loadChildren: './business/cmp-solution/cmp-solution.module#CmpSolutionModule'
+    },
+
+    {
+        path: 'tasks',
+        loadChildren: './business/cmp-task/cmp-task.module#CmpTaskModule'
+    },
+    {
+        path: 'comparison',
+        loadChildren: './business/comparison/comparison.module#ComparisonModule'
+    },
+    {
+        path: 'search',
+        loadChildren: './business/search/search.module#SearchModule'
+    },
+    {
+        path: 'help',
+        loadChildren: './business/help/help.module#HelpModule'
+    },
+    {
+        path: 'users/:username',
+        loadChildren: './business/profile/profile.module#ProfileModule'
+    },
+    {
+        path: 'test',
+        loadChildren: './business/test/test.module#TestModule'
+    },
+    {
         path: '**',
-        redirectTo: 'login',
+        redirectTo: 'home',
         pathMatch: 'full'
     }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {
-    useHash: true,
-    preloadingStrategy: PreloadAllModules
+    useHash: true
 });
