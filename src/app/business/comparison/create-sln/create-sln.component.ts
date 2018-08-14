6361d133d404502cd7cad75a4fdce4c50c395efd
shenchaoran <shenchaoran212@gmail.com>
2018-08-14 22:10:05 +0800

    。

--- src/app/business/comparison/create-sln/create-sln.component.ts
+++ src/app/business/comparison/create-sln/create-sln.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ogms-create-sln',
-  templateUrl: './create-sln.component.html',
-  styleUrls: ['./create-sln.component.scss']
+    selector: 'ogms-create-sln',
+    templateUrl: './create-sln.component.html',
+    styleUrls: ['./create-sln.component.scss']
 })
 export class CreateSlnComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
