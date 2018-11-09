48218713fa8a7c873bd1c07852694f5d7457a424
shenchaoran <shenchaoran212@gmail.com>
2018-11-10 00:16:15 +0800

    change: cmp-result-page

--- src/app/business/results/cmp-detail/cmp-detail.component.ts
+++ src/app/business/results/cmp-detail/cmp-detail.component.ts
@@ -7,7 +7,7 @@ import {
     ElementRef,
     Renderer2
 } from "@angular/core";
-import { TaskService, ConversationService } from '@services';
+import { TaskService, ConversationService, UserService } from '@services';
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { DynamicTitleService } from "@core/services/dynamic-title.service";
 import { ReactiveFormsModule } from "@angular/forms";
@@ -16,6 +16,7 @@ import { Observable, interval } from 'rxjs'
 import { map, switchMap, filter, tap, startWith } from 'rxjs/operators';
 import { CmpState } from '@models';
 import echarts from 'echarts';
+import { findIndex, get } from 'lodash';
 
 @Component({
     selector: 'ogms-cmp-detail',
@@ -24,13 +25,23 @@ import echarts from 'echarts';
     providers: [ConversationService]
 })
 export class CmpDetailComponent extends OgmsBaseComponent implements OnInit {
+    taskId;
     task;
+    ptMSs;
+    solution;
     hadTriggeredConversation;
+    echartDOMIndex = 0;
     @ViewChildren('echartDOM') echartDOM: QueryList<ElementRef>;
 
+    get user() { return this.userService.user; }
+    get users() { return this.conversationService.users; }
+    get couldEdit() { return this.user && this.solution && this.solution.auth.userId === this.user._id; }
+    get conversation() { return this.conversationService.conversation; }
+    get includeUser() { return findIndex(get(this, 'solution.subscribed_uids'), v => v === this.user._id) !== -1; }
     constructor(
         public route: ActivatedRoute,
         public taskService: TaskService,
+        public userService: UserService,
         private conversationService: ConversationService,
         public title: DynamicTitleService,
         public renderer2: Renderer2,
@@ -39,9 +50,13 @@ export class CmpDetailComponent extends OgmsBaseComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.taskService.findOne(this.route.snapshot.paramMap.get('id')).subscribe(res => {
-            if(!res.error) {
-                this.task = res.data;
+        this.taskId = this.route.snapshot.paramMap.get('id')
+        this.taskService.findOne(this.taskId).subscribe(res => {
+            if (!res.error) {
+                this.task = res.data.task;
+                this.ptMSs = res.data.ptMSs;
+                this.solution = res.data.solution;
+
                 this.fetchInterval();
             }
         });
@@ -50,47 +65,43 @@ export class CmpDetailComponent extends OgmsBaseComponent implements OnInit {
     private fetchInterval() {
         const record$ = interval(3000).pipe(
             switchMap((v, i) => {
-                return this.taskService.findOne(this.task._id, false);
-            }),
-            map(response => {
-                if (!response.error) {
-                    return response.data;
-                }
+                return this.taskService.findOne(this.taskId, false);
             })
         )
 
-        const subscription = record$.subscribe(doc => {
-            this.task = doc;
+        const subscription = record$.subscribe(res => {
+            this.task = res.data.task;
+            this.ptMSs = res.data.ptMSs;
+            this.solution = res.data.solution;
+
             if (this.task.state !== CmpState.RUNNING) {
                 subscription.unsubscribe();
                 if (this.task.state === CmpState.FINISHED_SUCCEED ||
                     this.task.state === CmpState.FINISHED_FAILED)
-                    this.buildChart();
+                    this.buildChart(0);
             }
         });
         this._subscriptions.push(subscription)
     }
 
-    private buildChart() {
-        let i = 0;
+    private buildChart(i) {
         setTimeout(() => {
-            this.task.cmpObjs.map(cmpObj => {
-                cmpObj.methods.map((method) => {
-                    if (method.id === '5b713f39a4857f1ba4be23ff') {
-                        if (method.result.state === CmpState.FINISHED_SUCCEED) {
-                            echarts
-                                .init(this.echartDOM.toArray()[i].nativeElement)
-                                .setOption(method.result);
-                        }
-                        i++;
+            let cmpObj = this.task.cmpObjs[i]
+            cmpObj.methods.map((method) => {
+                if (method.id === '5b713f39a4857f1ba4be23ff') {
+                    if (method.result.state === CmpState.FINISHED_SUCCEED) {
+                        echarts
+                            .init(this.echartDOM.toArray()[i].nativeElement)
+                            .setOption(method.result);
                     }
-                })
+                    this.echartDOMIndex++;
+                }
             })
         }, 10);
     }
-    
+
     onTabChange(index) {
-        if (index === 2 && !this.hadTriggeredConversation) {
+        if (index === this.task.cmpObjs.length && !this.hadTriggeredConversation) {
             this.conversationService.findOneByWhere({
                 pid: this.task._id
             }).subscribe(res => {
@@ -107,5 +118,23 @@ export class CmpDetailComponent extends OgmsBaseComponent implements OnInit {
                 }
             })
         }
+        else {
+            this.buildChart(index);
+        }
+    }
+
+    onSubscribeToggle() {
+        let ac = this.includeUser ? 'unsubscribe' : 'subscribe';
+        this.userService.toggleSubscribe('solution', ac, this.solution._id).subscribe(res => {
+            if (!res.error) {
+                let i = this.solution.subscribed_uids.findIndex(v => v === this.user._id);
+                if (ac === 'subscribe') {
+                    i === -1 && this.solution.subscribed_uids.push(this.user._id);
+                }
+                else if (ac === 'unsubscribe') {
+                    i !== -1 && this.solution.subscribed_uids.splice(i, 1);
+                }
+            }
+        });
     }
 }
